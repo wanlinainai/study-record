b08853d2a9c4c13c13e2f2d6c5171cdf7bd53249
zxh <zxh15166423670@gmail.com>
2025-04-27 00:46:42 +0800

    类

--- 前端学习/TS/index.js
+++ 前端学习/TS/index.js
@@ -1,37 +1,20 @@
 "use strict";
-let count;
-count = function (a, b) {
-    return a * b;
-};
-let arr;
-let arr2;
-arr = ['1', '2', '3'];
-arr2 = [1, 2, 3];
-let fuck1;
-let fuck2;
-let fuck3;
-console.log(1 /* Direction.Down */);
-function walk(str) {
-    if (str === 0 /* Direction.Up */) {
-        console.log('上');
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    else if (str === 1 /* Direction.Down */) {
-        console.log('下');
+    speak() {
+        console.log(`Hello, I'm ${this.name}, i'm ${this.age}岁`);
     }
-    else if (str === 2 /* Direction.Left */) {
-        console.log('左');
-    }
-    else if (str === 3 /* Direction.Right */) {
-        console.log('右');
-    }
-    console.log('MotherFucker');
 }
-function printStatus(data) {
-    console.log(data);
+const p1 = new Person('小明', '18');
+console.log(p1);
+p1.speak();
+// 属性的简写形式
+class Person2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 }
-function printGender(data) {
-    console.log(data);
-}
-printStatus(404);
-printStatus('404');
-printGender('男');
--- 前端学习/TS/index.ts
+++ 前端学习/TS/index.ts
@@ -1,59 +1,22 @@
-let count: (a: number, b: number) => number
-
-count = function (a, b) {
-  return a * b;
-}
-
-let arr: string[]
-let arr2: Array<number>
-
-arr = ['1', '2', '3']
-arr2 = [1, 2, 3]
-
-let fuck1: [string, number]
-let fuck2: [string, boolean?]
-let fuck3: [string, ...number[]]
-
-// 常量枚举
-const enum Direction {
-  Up,
-  Down,
-  Left,
-  Right
-}
-
-
-
-console.log(Direction.Down);
-
-function walk(str: Direction) {
-  if (str === Direction.Up) {
-    console.log('上');
-  } else if (str === Direction.Down) {
-    console.log('下');
-  } else if (str === Direction.Left) {
-    console.log('左');
-  } else if (str === Direction.Right) {
-    console.log('右');
+class Person {
+  name: string
+  age: string
+  constructor(name: string, age: string) {
+    this.name = name
+    this.age = age
   }
-  console.log('MotherFucker');
+
+  speak() {
+    console.log(`Hello, I'm ${this.name}, i'm ${this.age}岁`);
+  }
 }
 
-type shuzi = number
+const p1 = new Person('小明', '18')
+console.log(p1)
+p1.speak()
 
-type Status = number | string
-type Gender = '男' | '女'
-
-function printStatus(data: Status) {
-  console.log(data);
+// 属性的简写形式
+class Person2 {
+  constructor(public name: string, public age: number) { }
 }
-
-function printGender(data: Gender) {
-  console.log(data);
-}
-
-printStatus(404)
-printStatus('404')
-
-printGender('男')
 
