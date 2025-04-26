fb9aad61b09b9c2d503e79f02e45e9c1bffd72c6
zxh <zxh15166423670@gmail.com>
2025-04-27 00:15:23 +0800

    提交

--- 前端学习/TS/index.js
+++ 前端学习/TS/index.js
@@ -1,5 +1,37 @@
 "use strict";
-let a;
-let b;
-let c;
-a = 'Hello World';
+let count;
+count = function (a, b) {
+    return a * b;
+};
+let arr;
+let arr2;
+arr = ['1', '2', '3'];
+arr2 = [1, 2, 3];
+let fuck1;
+let fuck2;
+let fuck3;
+console.log(1 /* Direction.Down */);
+function walk(str) {
+    if (str === 0 /* Direction.Up */) {
+        console.log('上');
+    }
+    else if (str === 1 /* Direction.Down */) {
+        console.log('下');
+    }
+    else if (str === 2 /* Direction.Left */) {
+        console.log('左');
+    }
+    else if (str === 3 /* Direction.Right */) {
+        console.log('右');
+    }
+    console.log('MotherFucker');
+}
+function printStatus(data) {
+    console.log(data);
+}
+function printGender(data) {
+    console.log(data);
+}
+printStatus(404);
+printStatus('404');
+printGender('男');
--- 前端学习/TS/index.ts
+++ 前端学习/TS/index.ts
@@ -1,5 +1,59 @@
-let a:string
-let b:number
-let c:boolean
+let count: (a: number, b: number) => number
 
-a = 'Hello World'
+count = function (a, b) {
+  return a * b;
+}
+
+let arr: string[]
+let arr2: Array<number>
+
+arr = ['1', '2', '3']
+arr2 = [1, 2, 3]
+
+let fuck1: [string, number]
+let fuck2: [string, boolean?]
+let fuck3: [string, ...number[]]
+
+// 常量枚举
+const enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+
+
+console.log(Direction.Down);
+
+function walk(str: Direction) {
+  if (str === Direction.Up) {
+    console.log('上');
+  } else if (str === Direction.Down) {
+    console.log('下');
+  } else if (str === Direction.Left) {
+    console.log('左');
+  } else if (str === Direction.Right) {
+    console.log('右');
+  }
+  console.log('MotherFucker');
+}
+
+type shuzi = number
+
+type Status = number | string
+type Gender = '男' | '女'
+
+function printStatus(data: Status) {
+  console.log(data);
+}
+
+function printGender(data: Gender) {
+  console.log(data);
+}
+
+printStatus(404)
+printStatus('404')
+
+printGender('男')
+
